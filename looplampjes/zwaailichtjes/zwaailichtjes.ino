4e0873a3f1846e9ca427b755134a9838334fdf12
Jezzythekid <118938494+Jezzythekid@users.noreply.github.com>
2022-11-23 23:25:27 +0100

    Translation
    Fun little detail tho. in the Netherlands the flashing light on emergency verhicles are blue.

--- looplampjes/zwaailichtjes/zwaailichtjes.ino
+++ looplampjes/zwaailichtjes/zwaailichtjes.ino
@@ -1,11 +1,11 @@
 int vKnop = 0;
-int Knop = 13;
-int Schakelaar = 12;
+int Knop = 13;          //Knop = Button
+int Schakelaar = 12;    //Schakelaar = Switch
 int vSchakelaar = 0;
 int status = 1;
 
 void setup() {
-  // led's als output
+  // led's als output || led's as output
   for (int i = 2; i <= 11; i++) {
     pinMode(i, OUTPUT);
   }
@@ -13,7 +13,7 @@ void setup() {
   // Serial
   Serial.begin(9600);
 
-  //overig
+  //overig  || excess
   pinMode(Knop, INPUT);
   pinMode(Schakelaar, INPUT);
 }
@@ -27,18 +27,18 @@ void WaitRelease() {
 }
 
 void NightRider() {
-  // looplichtje zoals nightrider
+  // looplichtje zoals nightrider || animated light like nightrider
 
   for (int i = 2; i <= 9; i++) {
     digitalWrite(i, 1);
 
-    //verkomen van storing met andere Q's
+    //verkomen van storing met andere pin's || preventing disturbence with other pin's
     if (i < 8) {
     digitalWrite(i + 1, 1);
     digitalWrite(i + 2, 1);
     }
 
-    //de laatste led uitschakelen
+    //de laatste led uitschakelen || turn off previous led
     digitalWrite(i - 1, 0);
     delay(120);
   }
@@ -53,7 +53,7 @@ void NightRider() {
   digitalWrite(2, 0);
 }
 
-void Zwaailicht() {
+void Zwaailicht() {   //Zwaailicht = Flashing light (like on police cars and such.)
   for (int i = 0; i <= 5; i++) {
     digitalWrite(10, 1);
     delay(40);
